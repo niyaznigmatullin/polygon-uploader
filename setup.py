d68f8bf9827a88ca35aa6f226835a6a064fb575a
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2020-03-30 12:02:07 +0300

    Add url to setup

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setuptools.setup(
     name="lojacimport",
     version="0.1",
     description="loj.ac importer to polygon.codeforces.com",
+    url="https://github.com/niyaznigmatullin/lojacimport",
     author="Niyaz Nigmatullin",
     install_requires=[
         'polygon_client>=1.0a6',
