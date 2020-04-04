1aa0557f5bb6720c6b1c6bc696ce3a1eb9b23f5d
Niyaz Nigmatullin <niyaz.nigmatullin@gmail.com>
2020-04-04 13:53:02 +0300

    Add long_description and classifiers to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import setuptools
 import polygon_uploader
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="polygon-uploader",
     version=polygon_uploader.__version__,
     description="uploader to polygon.codeforces.com",
+    long_description=long_description,
     url="https://github.com/niyaznigmatullin/polygon-uploader",
     author="Niyaz Nigmatullin",
     install_requires=[
@@ -25,4 +29,9 @@ setuptools.setup(
             'usacoimport=polygon_uploader.usaco:main',
         ]
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
